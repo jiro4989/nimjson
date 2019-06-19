b9f1c9feb385d95c5992700cb8f3f96b58be1129
jiro4989 <jiroron666@gmail.com>
2019-06-19 14:25:59 +0900

    :fire: 使ってなかったコードなので削除した

--- src/nimjson/util.nim
+++ src/nimjson/util.nim
@@ -43,46 +43,6 @@ proc format*(self: JsonNode, objName = "Object"): string =
   else:
     discard
 
-proc toUgry*(result: var string, node: JsonNode) =
-  ## Converts `node` to its JSON Representation, without
-  ## regard for human readability. Meant to improve ``$`` string
-  ## conversion performance.
-  ##
-  ## JSON representation is stored in the passed `result`
-  ##
-  ## This provides higher efficiency than the ``pretty`` procedure as it
-  ## does **not** attempt to format the resulting JSON to make it human readable.
-  var comma = false
-  case node.kind:
-  of JArray:
-    result.add "["
-    for child in node.elems:
-      if comma: result.add ","
-      else:     comma = true
-      result.toUgly child
-    result.add "]"
-  of JObject:
-    result.add "{"
-    for key, value in pairs(node.fields):
-      if comma: result.add ","
-      else:     comma = true
-      key.escapeJson(result)
-      result.add ":"
-      result.toUgly value
-    result.add "}"
-  of JString:
-    node.str.escapeJson(result)
-  of JInt:
-    when defined(js): result.add($node.num)
-    else: result.addInt(node.num)
-  of JFloat:
-    when defined(js): result.add($node.fnum)
-    else: result.addFloat(node.fnum)
-  of JBool:
-    result.add(if node.bval: "true" else: "false")
-  of JNull:
-    result.add "null"
-
 when isMainModule:
   echo """{"str":"string1", "int":1, "float":1.15, "array":[1, 2, 3],
            "testObject":{"int":1, "obj2":{"str":"s", "bool2":true, "bool3":false},
