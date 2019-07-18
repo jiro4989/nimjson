80ecc9e5f10063c5890d6ecb28cba841548426b3
jiro4989 <jiroron666@gmail.com>
2019-07-18 18:32:52 +0900

    :fire: releaseに関係ないファイルを削除

--- examples/readfile/main.nim
+++
@@ -1,5 +0,0 @@
-import nimjson
-import json
-
-echo """{"keyStr":"str", "keyInt":1}""".parseJson().toTypeString()
-echo "../primitive.json".parseFile().toTypeString("testObject")
--- js/nimjson_js.nim
+++
@@ -1,5 +0,0 @@
-import nimjson
-import json
-
-proc generateNimDefinitions(str: cstring, publicField: bool): cstring {.exportc.} =
-  return str.`$`.parseJson().toTypeString(publicField = publicField)
