65e6fd57e82f36c2c65ba7880df0afe0be16abf6
jiro4989 <jiroron666@gmail.com>
2020-09-27 23:33:47 +0900

    chore: update version

--- src/nimjson.nim
+++ src/nimjson.nim
@@ -13,7 +13,7 @@ when not defined(js):
 
   const
     appName = "nimjson"
-    version = &"""{appName} command version 1.2.1
+    version = &"""{appName} command version 1.2.8
 Copyright (c) 2019 jiro4989
 Released under the MIT License.
 https://github.com/jiro4989/nimjson"""
