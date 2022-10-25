be5f277592d133b049ef679b6ccc3f977808e182
jiro4989 <jiroron666@gmail.com>
2022-10-26 08:59:07 +0900

    code format

--- src/nimjsonpkg/json_schema_parser.nim
+++ src/nimjsonpkg/json_schema_parser.nim
@@ -28,15 +28,18 @@ func typeStr(typ: string): string =
   of "null": "NilType"
   else: ""
 
-proc toObjectDefinitions(schema: JsonSchema, objectName: string, isPublic: bool, forceBackquote: bool): seq[ObjectDefinition] =
+proc toObjectDefinitions(schema: JsonSchema, objectName: string, isPublic: bool,
+    forceBackquote: bool): seq[ObjectDefinition] =
   var objDef = newObjectDefinition(objectName, false, isPublic, forceBackquote)
   for k, prop in schema.properties:
     let typ = prop.`type`
     let isOption = typ notin schema.required
-    let fDef = newFieldDefinition(typ.typeStr, typ, isPublic, forceBackquote, false, isOption)
+    let fDef = newFieldDefinition(typ.typeStr, typ, isPublic, forceBackquote,
+        false, isOption)
     objDef.addFieldDefinition(fDef)
   result.add(objDef)
 
-proc parse(s: string, objectName: string, isPublic: bool, forceBackquote: bool) =
+proc parse(s: string, objectName: string, isPublic: bool,
+    forceBackquote: bool) =
   let schema = s.fromJson(JsonSchema)
   let defs = schema.toObjectDefinitions(objectName, isPublic, forceBackquote)
