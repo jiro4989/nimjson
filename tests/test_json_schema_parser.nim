e5c256c6bcb5f81657b23c7cf87c830319bf9478
jiro4989 <jiroron666@gmail.com>
2022-11-01 01:58:36 +0900

    WIP: add testcode

--- tests/test_json_schema_parser.nim
+++ tests/test_json_schema_parser.nim
@@ -98,3 +98,70 @@ block:
     stream_seq: string
     deliveries: int64
     domain: string"""
+
+  block:
+    checkpoint "ok: nested object"
+    let j2 = """
+{
+  "$schema": "https://json-schema.org/draft/2020-12/schema",
+  "$id": "https://example.com/product.schema.json",
+  "title": "Product",
+  "description": "A product from Acme's catalog",
+  "type": "object",
+  "properties": {
+    "productId": {
+      "description": "The unique identifier for a product",
+      "type": "integer"
+    },
+    "productName": {
+      "description": "Name of the product",
+      "type": "string"
+    },
+    "price": {
+      "description": "The price of the product",
+      "type": "number",
+      "exclusiveMinimum": 0
+    },
+    "tags": {
+      "description": "Tags for the product",
+      "type": "array",
+      "items": {
+        "type": "string"
+      },
+      "minItems": 1,
+      "uniqueItems": true
+    },
+    "dimensions": {
+      "type": "object",
+      "properties": {
+        "length": {
+          "type": "number"
+        },
+        "width": {
+          "type": "number"
+        },
+        "height": {
+          "type": "number"
+        }
+      },
+      "required": [ "length", "width", "height" ]
+    }
+  },
+  "required": [ "productId", "productName", "price" ]
+}
+"""
+    let got = j2.parseAndGetString("Repository", false, false, true)
+#[
+
+    check got == """type
+  Repository = ref object
+    productId: int64
+    productName: string
+    price: float64
+    tags: seq[string]
+    dimensions: Dimensions
+  Dimensions = ref object
+    length: float64
+    width: float64
+    height: float64"""
+]#
