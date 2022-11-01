f2a336003aac6e72417aad3cabc4f2b09718e0b8
jiro4989 <jiroron666@gmail.com>
2022-11-02 08:41:04 +0900

    format code

--- examples/readfile/main.nim
+++ examples/readfile/main.nim
@@ -2,4 +2,5 @@ import nimjson
 
 echo """{"keyStr":"str", "keyInt":1}""".toTypeString()
 echo "../primitive.json".readFile().toTypeString("testObject")
-echo "../json_schema.json".readFile().toTypeString("testObject", jsonSchema = true)
+echo "../json_schema.json".readFile().toTypeString("testObject",
+    jsonSchema = true)
